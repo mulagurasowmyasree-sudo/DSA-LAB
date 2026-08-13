47673981c4b4e0d563ab0fe64b9dc76bb9891f1e
mulagurasowmyasree-sudo <mulagurasowmyasree@gmail.com>
2026-08-13 14:45:03 +0530

    Add files via upload

---
+++ WEEK2/binarysearch.py
@@ -0,0 +1,31 @@
+def binary_search(arr, key):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if arr[mid] == key:
+            return mid
+        elif arr[mid] < key:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return -1
+
+n = int(input("Enter the number of elements: "))
+arr = []
+
+print("Enter the elements in sorted order:")
+for i in range(n):
+    arr.append(int(input()))
+
+key = int(input("Enter the element to search: "))
+
+result = binary_search(arr, key)
+
+if result != -1:
+    print("Element found at index", result)
+else:
+    print("Element not found")
